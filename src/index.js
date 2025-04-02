18e583f907a21d98c246df7593a5e2366096db34
sheshank <sheshank3336@gmail.com>
2025-04-02 13:01:39 -0500

    initiated github pages

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+const repoName = "/my-portfolio-website";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={repoName}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
